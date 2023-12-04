9937b7901743365f8f4051b3cf34f85d9e0b2299
rastylus <ryan.p.riggs@gmail.com>
2023-12-04 04:43:22 -0600

    initial commit

---
+++ happyrootsjava/src/main/java/org/launchcode/happyrootsjava/HappyrootsjavaApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.happyrootsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyrootsjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyrootsjavaApplication.class, args);
+	}
+
+}
---
+++ happyrootsjava/src/test/java/org/launchcode/happyrootsjava/HappyrootsjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.happyrootsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyrootsjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
